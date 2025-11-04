a15a2574beb5416c9cef9611fa5ddd379ae7f799
Pauline <plinekaeorg@gmail.com>
2025-11-04 15:31:36 +0100

    Added an User object. With some code to put up some text on the console for the user to write their input.

--- JSONExercise1/Program.cs
+++ JSONExercise1/Program.cs
@@ -12,7 +12,21 @@ namespace JSONExercise1
     {
         static void Main(string[] args)
         {
-            
+            User user = new User();
+            {
+                //Skriver text för att vägleda användaren. Så att de kan skriva in sitt namn.
+                
+                Console.WriteLine("PLease write down your name:");
+                user.Name = Console.ReadLine();
+
+                //Skriver text härmed för att vägleda användaren. Så att de kan skriva in sin ålder.
+                //Däremot så kommer jag använda mig av TryParse ifall programmet skulle krascha.
+                //Så det är därför jag har med string input
+                Console.WriteLine("Please write down your age:");
+                string input = (Console.ReadLine());
+
+            }
+
         }
     }
 }
